04f21f480b8b9914e34261013a029f913bfa6f6f
이민홍 <rntmf0220@gmail.com>
2024-09-02 17:59:32 +0900

    [Update] 플레이어 움직임 추가

--- Assets/3.Script/Player/PlayerController.cs
+++ Assets/3.Script/Player/PlayerController.cs
@@ -1,7 +1,42 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+namespace Character
 {
-    
+    public class PlayerController : MonoBehaviour
+    {
+        private CharacterController characterController;
+        private Vector3 moveDirection;
+        private float moveSpeed = 4f;
+
+        private void Awake()
+        {
+            characterController = GetComponent<CharacterController>();
+        }
+
+        private void Update()
+        {
+            bool hasControl = (moveDirection != Vector3.zero);
+
+            if (hasControl)
+            {
+                transform.rotation = Quaternion.LookRotation(moveDirection);
+
+                characterController.Move(moveDirection * moveSpeed * Time.deltaTime);
+            }
+        }
+
+        private void OnMove(InputValue value)
+        {
+            Vector2 input = value.Get<Vector2>();
+            if (input != Vector2.zero)
+            {
+                moveDirection = new Vector3(input.x, 0f, input.y);
+            }
+            else
+            {
+                moveDirection = Vector3.zero;
+            }
+        }
+    }
 }
