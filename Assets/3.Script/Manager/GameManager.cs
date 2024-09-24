81e007991416d554e0412a84615d9c38af424feb
이민홍 <rntmf0220@gmail.com>
2024-09-24 20:22:22 +0900

    [Update] 네트워크 세션 테스트

--- Assets/3.Script/Manager/GameManager.cs
+++ Assets/3.Script/Manager/GameManager.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Fusion;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private NetworkRunner runner;
+    public TMP_InputField Roominput;
+
+    private void Start()
     {
-        
+        runner = gameObject.AddComponent<NetworkRunner>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CreateRoom()
     {
-        
+        StartGameArgs args = new StartGameArgs()
+        {
+            GameMode = GameMode.Server,
+            SessionName = Roominput.text == string.Empty ? "GameRoom" + Random.Range(0, 100) : Roominput.text,
+            PlayerCount = 4,
+            Scene = SceneRef.FromIndex(2)
+        };
+
+        runner.StartGame(args);
     }
+
 }
